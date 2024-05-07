21c4df05ff454fd464e1ae60139185f754d6232f
sudais2001 <162806729+sudais2001@users.noreply.github.com>
2024-05-08 00:59:43 +0500

    Add files via upload

---
+++ Favorite_fruit.ts
@@ -0,0 +1,21 @@
+let favourite_fruit : string [] = ["kivi","orange","apple"]
+
+if (favourite_fruit.includes("kivi")){
+   console.log("kivi")
+}
+
+if (favourite_fruit.includes("apple")){
+    console.log("apple")
+}
+
+if (favourite_fruit.includes("orange")){
+    console.log("orange")
+}
+
+if (favourite_fruit.includes("barry")){
+    console.log("You really like bananas")
+}
+
+if (favourite_fruit.includes("peach")){
+    console.log("You really like bananas")
+}
