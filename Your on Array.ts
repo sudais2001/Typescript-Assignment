2ffc03bf7948a493d060662c5e678683ee6f9019
sudais2001 <162806729+sudais2001@users.noreply.github.com>
2024-05-08 00:43:33 +0500

    Add files via upload

---
+++ Your on Array.ts
@@ -0,0 +1,7 @@
+
+
+let transportation :string[] = ['motorcae' , 'Car' , 'Train' , 'Cycle'] ;
+
+for(let i=0; i<transportation.length; i++){
+    console.log('i would ypu like on a ' + transportation[i])
+}
