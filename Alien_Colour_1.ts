88d5676b865f02765498f28ee951e84ca47ce796
sudais2001 <162806729+sudais2001@users.noreply.github.com>
2024-05-08 00:56:57 +0500

    Add files via upload

---
+++ Alien_Colour_1.ts
@@ -0,0 +1,17 @@
+
+//Creat a variable called alien_colour
+let alien_colour :string = 'green';
+
+//Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+if(alien_colour ==='green'){
+    console.log("player just earnd 5 points !");
+}
+
+//Write one version of this program that passes the if test and another that fails.
+ alien_colour = 'yello';
+ 
+ //(The version that fails will have no output.)
+if(alien_colour === 'greeen'){
+    console.log("player just earnd 5:points !")
+}
+
