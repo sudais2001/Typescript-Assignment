11d29a80db7c4a767093e89d9a3e3843d8c433ab
sudais2001 <162806729+sudais2001@users.noreply.github.com>
2024-05-08 00:58:14 +0500

    Add files via upload

---
+++ Alien_Colours_3.ts
@@ -0,0 +1,10 @@
+let alien_colour: string = "red"
+
+if(alien_colour == "green"){
+    console.log("5points")
+}else if (alien_colour == "yellow") {
+    console.log("10 points")
+}else {
+    console.log("15 points")
+}
+
