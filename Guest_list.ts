8da00a23ab7e204448af1620291f9983c690bc5c
sudais2001 <162806729+sudais2001@users.noreply.github.com>
2024-05-08 00:44:14 +0500

    Add files via upload

---
+++ Guest_list.ts
@@ -0,0 +1,7 @@
+let Guest_list :string[]= ['Nawaz Sharif' , 'Imran Khan' , 'Asif Zardari'];
+
+for(let i=0; i<Guest_list.length; i++){
+
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in your party.\n\nThank you!')
+
+}
