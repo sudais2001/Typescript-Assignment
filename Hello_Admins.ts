6510cc952436c25e235af6f70d108c4ba180e0d6
sudais2001 <162806729+sudais2001@users.noreply.github.com>
2024-05-08 01:00:20 +0500

    Add files via upload

---
+++ Hello_Admins.ts
@@ -0,0 +1,9 @@
+let users : string [] = ["admin","eric","Sudais","Raheel","Shahbaz"]
+
+for(let user of users){
+    if (user==="admin"){
+        console.log("Hello admin, would you like to see a status report?")
+    }else {
+        console.log(` Hello ${user}, thank you for logging in again`)
+    }
+}
