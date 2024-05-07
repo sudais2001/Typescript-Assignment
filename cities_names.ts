3ed1f225cd6bf3d801dd43ffb595a9979230f2a9
sudais2001 <162806729+sudais2001@users.noreply.github.com>
2024-05-08 01:06:33 +0500

    Add files via upload

---
+++ cities_names.ts
@@ -0,0 +1,12 @@
+function cityCountry(city: string, country: string): string{
+    return `${city}, ${country}`
+}
+
+let c1 = cityCountry('lahore', 'pakistan')
+let c2 = cityCountry('dhaka', 'Bangladesh')
+let c3 = cityCountry('paris', 'france')
+
+console.log(c1)
+console.log(c2)
+console.log(c3)
+
