ab39858a7144874c8eb559e428efdba9c60f39c0
sudais2001 <162806729+sudais2001@users.noreply.github.com>
2024-05-08 00:52:45 +0500

    Add files via upload

---
+++ International_Error.ts
@@ -0,0 +1,11 @@
+ //if you haven't recevied an array index error in one of your programs yet, try to make one happen.
+ //Creating array
+let newArray :string[] = ['Bill Gates', 'Imran khan', 'Baber azam'];
+
+//Accessing array by index
+console.log(newArray[5]);
+
+//make sure you correct the error before closing the program.
+
+//Correct the error
+console.log(newArray);
