d766f98726226d47fbdeef55fe95f117e0d08f67
sudais2001 <162806729+sudais2001@users.noreply.github.com>
2024-05-08 00:42:27 +0500

    Add files via upload

--- Names.ts
+++ Names.ts
@@ -2,8 +2,10 @@
 
 let names :string[] = ['Sudais','Basit','Azlan'] ;
 
+const message :string = 'Wellcome to Typescript class: '
+
 for(let i=0;i<names.length; i++){
    
-    console.log(names[i])
+    console.log(message + names[i])
 
 }
