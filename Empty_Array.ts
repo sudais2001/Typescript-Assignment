9ec004bee7a4491025024106867770df7da76d1a
sudais2001 <162806729+sudais2001@users.noreply.github.com>
2024-05-08 00:51:19 +0500

    Add files via upload

---
+++ Empty_Array.ts
@@ -0,0 +1,15 @@
+
+//Think of something you could store in a array.
+
+let items :string[] = ['Pakistan','Karachi','Urdu'];
+
+//Write a program that create a list contatining these items.
+
+let items2 : string[] = [];
+
+items2.push('Germeny');
+items2.push('Australia');
+items2.push('Dubai');
+
+
+console.log(items2);
