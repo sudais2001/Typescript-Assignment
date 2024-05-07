e010116b55625e1be78d48bda022dc7b9ff38a44
sudais2001 <162806729+sudais2001@users.noreply.github.com>
2024-05-08 01:01:00 +0500

    Add files via upload

---
+++ No_Users.ts
@@ -0,0 +1,9 @@
+import { URLSearchParams } from "url"
+
+let user : string []= ["eric","Sudais","Junaid","Admin"]
+
+
+user = []
+if (user.length === 0) {
+    console.log("we need to find some users!")
+}
