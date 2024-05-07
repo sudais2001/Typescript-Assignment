5b9e551189cb0199139d2f7871c21d9195bf7df9
sudais2001 <162806729+sudais2001@users.noreply.github.com>
2024-05-08 01:08:14 +0500

    Add files via upload

---
+++ great_magicians.ts
@@ -0,0 +1,11 @@
+function show_magicians(magicians: string[]): void{
+function make_great(magicians: string[]): void {
+    for (let i = 0; i < magicians.length; i++){
+        magicians[i] = magicians[i] + 'the great'
+}
+}
+}
+
+const magicians2: string[]= ["usman","haseeb","wajahat"]
+make_great(magicians2)
+show_magicians(magicians2)
